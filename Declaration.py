ff2eb478a48250de97afd55a6e583c072120acd5
cosmicNoot <155962034+Macc-Mp@users.noreply.github.com>
2024-02-17 22:42:22 +0800

    Declaring variables

---
+++ Declaration.py
@@ -0,0 +1,44 @@
+firstName = "Moises"
+lastName = "Paule"
+num = 20
+state = 65.90
+isState = True
+
+print(firstName,  lastName )
+print( "Age: ", num , "State: ", state, isState)
+
+#Input
+# print("Enter First Name: ")
+# fn = input()
+# print("Enter Last Name: ")
+# ln = input()
+#
+# name = "Full Name: " + fn + ln
+# print(name)
+
+
+# # Casting Variables, str turn int to String
+# Name = "MACH"
+# Num = 10
+#
+# print(Name + " " + str(Num))
+#
+# num1 = "1.5"
+# num2 = 2.5
+#
+# print(float(num1) + num2)
+
+#Division
+print(5/2)
+#Floor Division
+print(5//2)
+#Modulus take two 2 to to almost equal 5, which is 4
+print(5%2)
+#Exponent 5 raised to 2
+print(5**2)
+
+#Several Type Casting
+Addend1 = int(input("Enter Num1: "))
+Addend2 = int(input("Enter Num2: "))
+
+print("Sum", Addend1 + Addend2)
