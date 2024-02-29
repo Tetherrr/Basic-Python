4e3f34dc12db1646fb3b4b8a66ebbcf6249706e6
cosmicNoot <155962034+Macc-Mp@users.noreply.github.com>
2024-02-29 21:52:44 +0800

    OOP: Basic object and class instantiation

---
+++ Classes_Objects.py
@@ -0,0 +1,23 @@
+class Character:
+    name = "Name"
+    hp = 100
+    mp = 50
+    atk = 10
+    lvl = 1
+
+c1 = Character()
+c1.name = "Mox"
+c1.atk = 200
+c1.lvl = 10
+
+print(c1.name)
+
+class Product:
+    Name = "Eyeglass"
+    Product_Number = 220210
+    Price = 1500
+
+
+p1 = Product()
+print(p1.Name, p1.Product_Number, p1.Price)
+
