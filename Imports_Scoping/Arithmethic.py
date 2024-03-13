6fc87492adaf1c49ab131f1f9dc3e0cabb34e1b6
cosmicNoot <155962034+Macc-Mp@users.noreply.github.com>
2024-03-13 21:58:05 +0800

    Handle several Files

---
+++ Imports_Scoping/Arithmethic.py
@@ -0,0 +1,17 @@
+def Add(x, y):
+    sum =x + y
+    return sum
+
+def Subtract(x, y):
+    diff = x - y
+    return diff
+
+def Multiply(x, y):
+    product = x * y
+    return product
+
+def Division(x, y):
+    quotient = x - y
+    return quotient
+
+
---
+++ Imports_Scoping/Main.py
@@ -0,0 +1,8 @@
+from Arithmethic import Add
+from Username import User as Un
+
+sum1 = Add(5, 5)
+print(sum1)
+
+User1 = Un("MOISES", 20)
+User1.call()
